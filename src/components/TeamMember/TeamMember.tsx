9432806fe02a2d69bd557f0f2136dfd6487724ba
brendacs <brenda.zhang2@gmail.com>
2021-03-12 08:45:55 -0800

    Add new mods, improve picture alignment

--- src/components/TeamMember/TeamMember.tsx
+++ src/components/TeamMember/TeamMember.tsx
@@ -3,7 +3,6 @@ import classnames from 'classnames';
 import styles from './TeamMember.module.scss';
 
 interface TeamMemberProps {
-  name: string;
   username: string;
   userRole: string;
   active: boolean;
@@ -11,14 +10,13 @@ interface TeamMemberProps {
 }
 
 const TeamMember = (props: TeamMemberProps): ReactElement => {
-  const { name, username, userRole, active, imageSrc } = props;
+  const { username, userRole, active, imageSrc } = props;
   return (
     <div className={classnames(styles.teamMember)}>
       <div className={styles.imageContainer}>
         {imageSrc && <img src={imageSrc} width={250} height={250} alt="CSCH team member profile" loading="lazy" />}
       </div>
       <div className={styles.infoContainer}>
-        <p className={styles.name}>{name}</p>
         <p className={styles.username}>{username}</p>
         <p className={styles.userRole}>{userRole}</p>
         <div className={active ? 'active' : 'inactive'} />
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -6,44 +6,17 @@ import styles from './team.module.scss';
 
 // TODO: Pull data from Discord API instead
 const teamMembers = [
-  {
-    id: 1,
-    name: 'Kevin',
-    username: 'Kevin',
-    userRole: 'Server Owner',
-    active: true
-  },
+  { id: 1, username: 'Kevin', userRole: 'Server Owner', active: true },
   {
     id: 2,
-    name: 'Brenda',
     username: 'Brenda',
     userRole: 'Server Partner, Bot & Web Developer',
     active: true,
     imageSrc: '/images/team/brenda.gif'
   },
-  {
-    id: 3,
-    name: 'Nik',
-    username: 'Nik',
-    userRole: 'Server Admin, Bot Developer',
-    active: true,
-    imageSrc: '/images/team/nik.png'
-  },
-  {
-    id: 3,
-    name: 'Peter',
-    username: 'Peter',
-    userRole: 'Server Admin',
-    active: true
-  },
-  {
-    id: 4,
-    name: 'Bri',
-    username: 'iNirvana',
-    userRole: 'Header Server Mod',
-    active: true,
-    imageSrc: '/images/team/inirvana.png'
-  },
+  { id: 3, username: 'Nik', userRole: 'Server Admin, Bot Developer', active: true, imageSrc: '/images/team/nik.png' },
+  { id: 3, username: 'Peter', userRole: 'Server Admin', active: true },
+  { id: 4, username: 'iNirvana', userRole: 'Header Server Mod', active: true, imageSrc: '/images/team/inirvana.png' },
   { id: 5, username: 'Gopher', userRole: 'Head Server Mod', active: true, imageSrc: '/images/team/gopher.png' },
   { id: 6, username: 'Jeremy', userRole: 'Head Server Mod', active: true, imageSrc: '/images/team/jeremy.png' },
   { id: 7, username: 'Wickler', userRole: 'Head Server Mod', active: true, imageSrc: '/images/team/wickler.png' },
@@ -58,18 +31,15 @@ const teamMembers = [
   { id: 10, username: 'Manny', userRole: 'Server Mod', active: true, imageSrc: '/images/team/manny.png' },
   { id: 12, username: 'The Coconut Meme', userRole: 'Server Mod', active: true, imageSrc: '/images/team/coconut.png' },
   { id: 13, username: 'Whiplash', userRole: 'Server Mod', active: true, imageSrc: '/images/team/whiplash.png' },
-  {
-    id: 14,
-    name: 'John',
-    username: 'Morgos',
-    userRole: 'Server Owner',
-    active: false,
-    imageSrc: '/images/team/john.png'
-  },
+  { id: 14, username: 'Morgos', userRole: 'Server Owner', active: false, imageSrc: '/images/team/john.png' },
   { id: 15, username: 'asyncawait', userRole: 'Server Admin', active: false, imageSrc: '/images/team/async.png' },
   { id: 16, username: 'enufstuff', userRole: 'Server Admin', active: false },
   { id: 17, username: 'mauri', userRole: 'Server Admin', active: false, imageSrc: '/images/team/mauri.png' },
   { id: 18, username: 'veremit', userRole: 'Server Admin', active: false, imageSrc: '/images/team/veremit.png' },
+  { id: 19, username: 'ender', userRole: 'Server Mod', active: true, imageSrc: '/images/team/ender.png' },
+  { id: 19, username: 'bofh', userRole: 'Server Mod', active: true, imageSrc: '/images/team/bofh.png' },
+  { id: 19, username: 'ff', userRole: 'Server Mod', active: true, imageSrc: '/images/team/ff.png' },
+  { id: 19, username: 'vcarl', userRole: 'Server Mod', active: true, imageSrc: '/images/team/vcarl.png' },
   { id: 19, username: 'amicus_elit', userRole: 'Server Mod', active: false, imageSrc: '/images/team/amicus.png' },
   { id: 20, username: 'csgator', userRole: 'Server Mod', active: false },
   { id: 21, username: 'KensoDev', userRole: 'Server Mod', active: false, imageSrc: '/images/team/kenso.png' },
@@ -89,11 +59,10 @@ const Team = (): ReactElement => (
           {teamMembers.map(member => (
             <TeamMember
               key={member.id}
-              name={member?.name}
-              username={member?.username}
+              username={member.username}
               userRole={member.userRole}
               active={member.active}
-              imageSrc={member?.imageSrc}
+              imageSrc={member.imageSrc}
             />
           ))}
         </div>
