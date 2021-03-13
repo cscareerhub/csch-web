9cca60f4e7a71298d88999ea14b3f66aaf54bc9b
brendacs <brenda.zhang2@gmail.com>
2021-03-13 11:16:10 -0800

    Improve contact form and add required attr to message field, update success message, clean up some CSS variables, globals, and media queries

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -5,8 +5,10 @@ const ContactForm = (): ReactElement => (
   <form className={styles.contactForm} name="contact" method="POST" action="/success" data-netlify="true">
     <input type="hidden" name="form-name" value="contact" />
     <input type="text" name="name" id="name" placeholder="Name" />
+    <input type="text" name="username" id="username" placeholder="Username#ID" />
     <input type="email" name="email" id="email" placeholder="Email" />
-    <textarea name="message" id="message" placeholder="Message" />
+    <textarea name="message" id="message" placeholder="Message" required />
+    <p className={styles.requiredNotation}>*</p>
     <button type="submit">Send</button>
   </form>
 );
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -5,7 +5,11 @@ import styles from './success.module.scss';
 const Success = (): ReactElement => (
   <Layout>
     <div className={styles.successContainer}>
-      <p>Form successfully submitted!</p>
+      <p>Message successfully submitted!</p>
+      <p>
+        If you sent a message that requires a reply, watch out for a message on Discord if you used your username with
+        an ID. Otherwise, we'll write back to the email provided.
+      </p>
     </div>
   </Layout>
 );
