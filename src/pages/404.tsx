dd4245146ee08960bc0a86e7085b52d217756e2a
brendacs <brenda.zhang2@gmail.com>
2021-03-13 10:59:43 -0800

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import Layout from '@components/Layout';
+import React, { ReactElement } from 'react';
+import styles from './404.module.scss';
+
+const NotFound = (): ReactElement => (
+  <Layout>
+    <div className={styles.successContainer}>
+      <p>404 | Page Not Found</p>
+    </div>
+  </Layout>
+);
+
+export default NotFound;
