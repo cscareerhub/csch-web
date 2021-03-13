75ba9b0cf7c8998c174dfdbdaf9a1ff924efcf6b
brendacs <brenda.zhang2@gmail.com>
2021-03-13 11:18:57 -0800

    Update naming of class in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import styles from './404.module.scss';
 
 const NotFound = (): ReactElement => (
   <Layout>
-    <div className={styles.successContainer}>
+    <div className={styles.notFoundContainer}>
       <p>404 | Page Not Found</p>
     </div>
   </Layout>
