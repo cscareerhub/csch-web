751e11f67e4c534a1529fb3ba0321335b6dee8cf
brendacs <brenda.zhang2@gmail.com>
2021-06-07 09:40:37 -0700

    [fix] Fix mixed up styles

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -19,7 +19,8 @@ const HeadComponent: React.FC<HeadProps> = props => {
     <Head>
       <title>{metaTitle}</title>
       <link rel="icon" href="/csch-statico-round.png" />
-      <link href="https://fonts.googleapis.com/css?family=Inter|Roboto" rel="stylesheet" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900" rel="stylesheet" />
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=1" />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ const Header = (props: HeaderProps): ReactElement => {
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
-        <h1 className={styles.title}>{title}</h1>
+        <h1 className={styles.title} id ="header-title">{title}</h1>
         <p className={styles.text}>{text}</p>
         {actionText && <ActionButton actionText={actionText} link="https://discord.gg/cscareerhub" />}
       </div>
