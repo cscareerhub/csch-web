8a0ed06ec5ad11c7de1b77cf78f3ba4b75c1100b
brendacs <brenda.zhang2@gmail.com>
2021-06-09 21:28:14 -0700

    [misc] Add top button to MdLayout and minor content edit

--- src/components/MdLayout/MdLayout.tsx
+++ src/components/MdLayout/MdLayout.tsx
@@ -6,16 +6,31 @@ import styles from './MdLayout.module.scss';
 interface LayoutProps {
   children: JSX.Element | JSX.Element[];
   title: string;
+  hideTopButton: boolean;
 }
 
 const MdLayout = (props: LayoutProps): ReactElement => {
-  const { children, title } = props;
+  const { children, title, hideTopButton } = props;
   return (
     <>
       <HeadComponent title={title} />
       <Layout>
         <div className={styles.container}>
-          <div className={styles.content}>{children}</div>
+          <div className={styles.content}>
+            {children}
+            {!hideTopButton && (
+              <div className="topButton">
+                <a
+                  onClick={() => window.scrollTo({ top: 0 })}
+                  onKeyDown={() => window.scrollTo({ top: 0 })}
+                  role="button"
+                  tabIndex={0}
+                >
+                  Go to top
+                </a>
+              </div>
+            )}
+          </div>
         </div>
       </Layout>
     </>
