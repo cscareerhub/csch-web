b9be8e8961290a06c6068e2d8896d29c9054718f
brendacs <brenda.zhang2@gmail.com>
2021-06-15 21:24:36 -0700

    [misc] Update team page order, section descriptions, change inactive to hall of fame

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -9,81 +9,119 @@ const Team = ({ teamMembers }): ReactElement => (
   <>
     <HeadComponent title="Team" />
     <Layout>
-      <h1 className={styles.teamHeading}>The CS Career Hub Team</h1>
+      <div className={styles.teamPageWrapper}>
+        <h1 className={styles.teamHeading}>The CS Career Hub Team</h1>
 
-      <div className={styles.teamMembers}>
-        {teamMembers
-          .filter(member => member.fields.Active)
-          .filter(
-            member =>
-              member.fields['User Role'].includes('Lead') ||
-              member.fields['User Role'].includes('Director') ||
-              member.fields['User Role'].includes('Partner') ||
-              member.fields['User Role'].includes('Owner')
-          )
-          .sort((member1, member2) => (member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1))
-          .map(member => (
-            <TeamMember
-              key={member.id}
-              username={member.fields.Name}
-              userRole={member.fields['User Role']}
-              image={member.fields.Image ? member.fields.Image[0]?.url : null}
-              active={member.fields.Active}
-            />
-          ))}
-      </div>
+        <div className={styles.teamMembers}>
+          {teamMembers
+            .filter(member => member.fields.Active)
+            .filter(member => member.fields['User Role'].includes('CEO') || member.fields['User Role'].includes('COO'))
+            .sort((member1, member2) =>
+              member1.fields['User Role'].toLowerCase() > member2.fields['User Role'].toLowerCase() ? 1 : -1
+            )
+            .map(member => (
+              <TeamMember
+                key={member.id}
+                username={member.fields.Name}
+                userRole={member.fields['User Role']}
+                image={member.fields.Image ? member.fields.Image[0]?.url : null}
+                active={member.fields.Active}
+              />
+            ))}
 
-      <h1 className={styles.teamHeading}>Server Moderators</h1>
+          {teamMembers
+            .filter(member => member.fields.Active)
+            .filter(
+              member => member.fields['User Role'].includes('Lead') || member.fields['User Role'].includes('Director')
+            )
+            .sort((member1, member2) =>
+              member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1
+            )
+            .map(member => (
+              <TeamMember
+                key={member.id}
+                username={member.fields.Name}
+                userRole={member.fields['User Role']}
+                image={member.fields.Image ? member.fields.Image[0]?.url : null}
+                active={member.fields.Active}
+              />
+            ))}
+        </div>
 
-      <div className={styles.teamMembers}>
-        {teamMembers
-          .filter(member => member.fields.Active)
-          .filter(member => member.fields['User Role'].includes('Server Mod'))
-          .sort((member1, member2) => (member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1))
-          .map(member => (
-            <TeamMember
-              key={member.id}
-              username={member.fields.Name}
-              userRole={member.fields['User Role']}
-              image={member.fields.Image ? member.fields.Image[0]?.url : null}
-              active={member.fields.Active}
-            />
-          ))}
-      </div>
+        <h1 className={styles.teamHeading}>Server Moderators</h1>
 
-      <h1 className={styles.teamHeading}>Project Contributors</h1>
+        <p className={styles.teamDescription}>
+          Our moderators often double as full community managers. They not only help keep the community in order and
+          provide support to members, but also brainstorm ideas, execute initiatives to improve the community, and
+          contribute greatly to events and projects.
+        </p>
 
-      <div className={styles.teamMembers}>
-        {teamMembers
-          .filter(member => member.fields.Active)
-          .filter(member => member.fields['User Role'].includes('Contributor'))
-          .sort((member1, member2) => (member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1))
-          .map(member => (
-            <TeamMember
-              key={member.id}
-              username={member.fields.Name}
-              userRole={member.fields['User Role']}
-              image={member.fields.Image ? member.fields.Image[0]?.url : null}
-              active={member.fields.Active}
-            />
-          ))}
-      </div>
+        <div className={styles.teamMembers}>
+          {teamMembers
+            .filter(member => member.fields.Active)
+            .filter(member => member.fields['User Role'].includes('Server Mod'))
+            .sort((member1, member2) =>
+              member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1
+            )
+            .map(member => (
+              <TeamMember
+                key={member.id}
+                username={member.fields.Name}
+                userRole={member.fields['User Role']}
+                image={member.fields.Image ? member.fields.Image[0]?.url : null}
+                active={member.fields.Active}
+              />
+            ))}
+        </div>
 
-      <h1 className={styles.teamHeading}>Inactive</h1>
+        <h1 className={styles.teamHeading}>Project Contributors</h1>
 
-      <div className={styles.teamMembers}>
-        {teamMembers
-          .filter(member => !member.fields.Active)
-          .sort((member1, member2) => (member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1))
-          .map(member => (
-            <TeamMember
-              key={member.id}
-              username={member.fields.Name}
-              userRole={member.fields['User Role']}
-              image={member.fields.Image ? member.fields.Image[0]?.url : null}
-              active={member.fields.Active}
-            />
-          ))}
+        <p className={styles.teamDescription}>
+          Contributors lend a helping hand wherever necessary and have made many of our events, development projects,
+          and community intiatives possible!
+        </p>
+
+        <div className={styles.teamMembers}>
+          {teamMembers
+            .filter(member => member.fields.Active)
+            .filter(member => member.fields['User Role'].includes('Contributor'))
+            .sort((member1, member2) =>
+              member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1
+            )
+            .map(member => (
+              <TeamMember
+                key={member.id}
+                username={member.fields.Name}
+                userRole={member.fields['User Role']}
+                image={member.fields.Image ? member.fields.Image[0]?.url : null}
+                active={member.fields.Active}
+              />
+            ))}
+        </div>
+
+        <h1 className={styles.teamHeading}>Hall of Fame</h1>
+
+        <p className={styles.teamDescription}>
+          A section to honor those who are no longer an active part of the CSCH staff, but have made great contributions
+          to the community.
+        </p>
+
+        <div className={styles.teamMembers}>
+          {teamMembers
+            .filter(member => !member.fields.Active)
+            .sort((member1, member2) =>
+              member1.fields.Name.toLowerCase() > member2.fields.Name.toLowerCase() ? 1 : -1
+            )
+            .map(member => (
+              <TeamMember
+                key={member.id}
+                username={member.fields.Name}
+                userRole={member.fields['User Role']}
+                image={member.fields.Image ? member.fields.Image[0]?.url : null}
+                active={member.fields.Active}
+              />
+            ))}
+        </div>
       </div>
     </Layout>
   </>
