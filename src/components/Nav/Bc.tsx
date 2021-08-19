7b15245f42d885f9edbd8fb55de99fcdf11fa30c
sif <sifer.aseph@nyu.edu>
2021-08-19 03:32:59 -0400

    [feature] Breadcrumbs programmatically created

---
+++ src/components/Nav/Bc.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Breadcrumbs from 'nextjs-breadcrumbs';
+
+function csch_names(title) {
+  switch(title) {
+    case 'event calendar':
+      return "Event Calendar"
+    case 'blog':
+      return "Blog"
+    case 'community rules':
+      return "Community Rules"
+    case 'code of conduct':
+      return "Code of Conduct"
+    case 'career resources':
+      return "Career Resources"
+    case 'wiki faq':
+      return "Wiki & FAQ"
+    case 'information career':
+      return "Information & Career"
+    case 'links resources':
+      return "Links & Resources"
+    case 'csch resources':
+      return "CSCH Resources"
+    case 'csch links':
+      return "CSCH Links"
+    case 'contact':
+      return "Contact"
+    default:
+      return title
+  }
+}
+
+const LittleCrumbs = () => {
+  return <Breadcrumbs
+          transformLabel={(title) => csch_names(title)}
+          listClassName={"littlecrumbs"}
+          omitRootLabel
+          />;
+};
+
+export default LittleCrumbs;
