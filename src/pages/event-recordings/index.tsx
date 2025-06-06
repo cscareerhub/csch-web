be94a3454bb8c57c53c0a3da3357e871e6fc9bc9
Miley <68884789+Miley-CS@users.noreply.github.com>
2025-06-05 20:11:49 -0700

    Update and rename blog to event recordings (#57)

--- src/pages/event-recordings/index.tsx
+++ src/pages/event-recordings/index.tsx
@@ -4,7 +4,7 @@ import { getSortedPostsData } from '../../lib/getPosts';
 import { PostData } from '../../models/Post';
 import styles from './index.module.scss';
 
-const directory = 'blog';
+const directory = 'event-recordings';
 
 export async function getStaticProps(): Promise<Record<'props', any>> {
   const allPostsData = getSortedPostsData(directory);
@@ -15,11 +15,11 @@ export async function getStaticProps(): Promise<Record<'props', any>> {
   };
 }
 
-const BlogPosts = (props): ReactElement => {
+const EventRecordings = (props): ReactElement => {
   const { allPostsData } = props;
   return (
     <>
-      <h1>Blog Posts</h1>
+      <h1>Event Recordings</h1>
       {allPostsData.map((data: PostData) => (
         <li key={data?.id} className={styles.noListStyle}>
           <span>{data?.date}</span>
@@ -32,6 +32,6 @@ const BlogPosts = (props): ReactElement => {
   );
 };
 
-export default BlogPosts;
+export default EventRecordings;
 
-BlogPosts.displayName = 'Blog';
+EventRecordings.displayName = 'Event Recordings';
