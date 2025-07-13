204e37bc0db59477271bc70f5afe2e783e1c6e09
Miley-CS <mileyq88@gmail.com>
2025-07-13 14:11:03 -0700

    Adressed review comments and make section responsive

--- src/components/Testimonials/Testimonial/Testimonial.tsx
+++ src/components/Testimonials/Testimonial/Testimonial.tsx
@@ -3,16 +3,15 @@ import styles from './Testimonial.module.scss';
 import classnames from 'classnames';
 
 interface TestimonialProps {
-    className: string;
     text: string;
     occupation: string;
 }
 
 const Testimonial = (props: TestimonialProps): ReactElement => {
-    const { className, text, occupation } = props;
+    const {text, occupation } = props;
     return (
         <div className={styles.testimonial}>
-            <p className={classnames(styles.quoteOne, styles.quotes)}>&ldquo;</p>
+            <p className={styles.quotes}>&ldquo;</p>
             <p className={styles.text}>{text}</p>
             <p className={classnames(styles.quote, styles.quotes)}>&rdquo;</p>
             <div>
